9e5c674b0592597acbfc3c0dfb9740d17ac1f827
Mac <mac@Macs-MacBook-Pro.local>
2023-12-28 17:00:04 +0530

    add invoice and order

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { userController } from './user.controller';
 import { userService } from './user.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { OrderModule } from './order/order.module';
+import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController,userController],
-  providers: [AppService,userService],
+  imports: [OrderModule, InvoiceModule],
+  controllers: [AppController,userController, ProductController],
+  providers: [AppService,userService, ProductService],
 })
 export class AppModule {}
